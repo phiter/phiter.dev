364a944a439b31b7208f5a7b611b9180e011903c
Phiter Fernandes <phiterfernandes@gmail.com>
2019-10-28 23:21:38 -0300

    perf: adds defer to webfont

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Raleway&display=swap'
+        href: 'https://fonts.googleapis.com/css?family=Raleway&display=swap',
+        defer: true
       }
     ]
   },
